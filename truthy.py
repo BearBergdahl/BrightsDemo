1bf570ff243d5d159117e0e1a836b8b313e71896
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2024-06-19 08:23:15 +0200

    Added truthy exercise

---
+++ truthy.py
@@ -0,0 +1,16 @@
+
+# Try them out
+empty_int = int()
+empty_string = str()
+empty_bool = bool()
+empty_float = float()
+
+empty_list = list()
+empty_set = set()
+
+if empty_int:
+    print('It is true')
+else:
+    print('It is false')
+
+# Then replace with values, 0, '', [] and actual strings numbers list and so on.
