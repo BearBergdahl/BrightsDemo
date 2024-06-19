9964965776cca59cee970f5b7c425da245712c0f
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2024-06-19 08:51:25 +0200

    truthy and scope

---
+++ scope.py
@@ -0,0 +1,22 @@
+everywhere = 9
+
+def my_func(num):
+    somewhere = 4
+    print(num * somewhere)
+
+my_func(everywhere)
+print(everywhere)
+
+for i in range(10):
+    a_num = 5
+    print(i * a_num)
+
+print(a_num)
+
+if True:
+    a_word = 'word'
+    _private = 8
+    print(a_word)
+
+print(a_word)
+print(_private)
--- truthy.py
+++ truthy.py
@@ -8,9 +8,15 @@ empty_float = float()
 empty_list = list()
 empty_set = set()
 
-if empty_int:
+if []:
     print('It is true')
 else:
     print('It is false')
 
 # Then replace with values, 0, '', [] and actual strings numbers list and so on.
+
+is_populated = [5,6]
+if len(is_populated) > 0:
+    print('do stuff')
+else:
+    print('is_populated is false')
